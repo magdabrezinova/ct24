54e5734c112e7bc93acb1dfda8bf9c001eaceddc
Magda Březinová <94903874+magdabrezinova@users.noreply.github.com>
2026-03-03 06:14:20 +0100

    Refactor ct24.spec.ts for improved test structure

--- tests/ct24.spec.ts
+++ tests/ct24.spec.ts
@@ -1,37 +1,27 @@
-import { test, expect } from '@playwright/test';
+import { test } from '@playwright/test';
 import { HomePage } from '../pages/HomePage';
 
-test('Nejčtenější články', async ({ page }) => {
+test('Nejčtenější články >= šířka 768px', async ({ page }) => {
   const home = new HomePage(page);
-  await page.goto('https://ct24.cz');
-
-  const links = page.locator('[aria-label="Rubriky"] a');
-
-  const rubriky = await links.evaluateAll(els =>
-    els.map(el => ({
-      url: (el as HTMLAnchorElement).href,
-      text: el.textContent?.trim()
-    }))
-  );
-
-  async function checkArticles(rubrika: string){
-    await page.goto(rubrika, { waitUntil: 'domcontentloaded' });
-    await home.expectRubrikaUrl(rubrika);
-    await home.waitForLayout();
-    //Nejčtenější články jsou nad patičkou
-    await home.expectNejctenejsiAboveFooter();
-    //Nahrají se články s odkazy
-    await home.expectArticlesLoaded(4);
-    //Články obsahují titulky a první tři články mají fotky
-    await home.expectArticlesHaveTitlesAndThreeImages();
-  }
-
-  for (const rubrika of rubriky) {
-    await checkArticles(rubrika.url);
-    await home.goTo7Dni();
-    await checkArticles(rubrika.url);
-    await home.goTo24Hodin();
-    await checkArticles(rubrika.url);
-  }
+  await home.testMostReadDesktop();
 });
 
+test('Nejčtenější články < šířka 768px', async ({ page }) => {
+  const home = new HomePage(page);
+  await home.testMostReadMobile();
+});
+
+test('Rubrika Domácí', async ({ page }) => {
+  const home = new HomePage(page);
+  await home.testRubrikaDomaci();
+});
+
+test('Responsivní design', async ({ page }) => {
+  const home = new HomePage(page);
+  await home.testResponsiveDesign();
+});
+
+test('Živé vysílání', async ({ page, browserName }) => {
+  const home = new HomePage(page);
+  await home.testLiveBroadcast(browserName);
+});
