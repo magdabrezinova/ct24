6971ef8f198c7a30d30c2846046a13f844577c54
Magda Březinová <94903874+magdabrezinova@users.noreply.github.com>
2026-03-03 06:15:01 +0100

    Disable fullyParallel and update timeout settings
    Disabled fully parallel test execution and adjusted timeouts.

--- playwright.config.ts
+++ playwright.config.ts
@@ -14,7 +14,7 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
@@ -23,13 +23,17 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/
+  class-testoptions. */
+  timeout: 480000,
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on',
+    actionTimeout: 120000,  
+    navigationTimeout: 120000,
   },
 
   /* Configure projects for major browsers */
